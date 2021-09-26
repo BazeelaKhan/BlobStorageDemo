a2f985b02f9661f2e6a1d9123113a173644d284c
BazeelaKhan <bazeela_khan@persistent.com>
2021-09-26 20:22:49 +0530

    TestCase for - if image is not exists in container

--- BlobStorageDemo/ImageService.cs
+++ BlobStorageDemo/ImageService.cs
@@ -40,7 +40,7 @@ namespace BlobStorageDemo
             }
             catch (Exception ex)
             {
-
+               
             }
             return null;
         }
--- UnitTesting/Class1.cs
+++ UnitTesting/Class1.cs
@@ -1,14 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
+using BlobStorageDemo;
+using Moq;
 using NUnit;
+using NUnit.Framework;
 
 namespace UnitTesting
-{
+{   
+    [TestFixture]
     public class Class1
     {
+        [Test]
+        public async Task IsImageExists_IfNotExists_ReturnUrl()
+        {
+            //Arrange
+            ImageService imageService = new ImageService();
+            HttpPostedFileBase httpPostedFile = Mock.Of<HttpPostedFileBase>();
+            var mock = Mock.Get(httpPostedFile);
+            mock.Setup(_ => _.FileName).Returns("fakeFileName.extension");
+
+            //Act
+            var result = await imageService.IsImageExists(httpPostedFile);
+
+            //Assert
+
+            Assert.AreEqual(null, result);
+            
+        }
 
     }
 }
